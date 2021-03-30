cc7f7b63cbe91c83b7a47bf0e55e07c694f39415
Vlad Postolachi <postolachi.vlad@gmail.com>
2021-03-30 12:27:53 +0200

    Create Link model validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,18 @@
 class Link < ApplicationRecord
+
+    validates :original_url, :url_short_code, presence: true
+    validates :url_short_code, uniqueness: true
+    validate :validate_url
+
+
+    def validate_url
+        begin
+            uri = URI.parse(original_url)
+            if uri.host.nil?
+                errors.add(:url, "Invalid URL format")
+            end
+        rescue URI::InvalidURIError => e
+        end
+    end
+
 end
